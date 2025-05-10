a14e75af2b73451fc0cb6fe8f1f279b4bc67fcb5
futzone <futzoneuz@gmail.com>
2025-05-10 21:47:11 +0500

    the app btn add

--- src/data/cta.ts
+++ src/data/cta.ts
@@ -1,6 +1,7 @@
 export const ctaDetails = {
     heading: 'O‘z ona tilingiz imlosiga befarq bo‘lmang!',
     subheading: '«Imlo Go» orqali o‘zbek tili qadriyatlari va imlosi borasidagi bilimlaringizni mustahkamlang, bilimingizni tekshirib ko‘ring.',
-    appStoreUrl: '#',
-    googlePlayUrl: '#'
+    appStoreUrl: 'https://jvqhqinpulczyhgckgbb.supabase.co/storage/v1/object/public/imlogo-audios/apps/Imlogo_ios.zip',
+    googlePlayUrl: 'https://jvqhqinpulczyhgckgbb.supabase.co/storage/v1/object/public/imlogo-audios/apps/imlogo.apk',
+    windowsUrl: 'https://jvqhqinpulczyhgckgbb.supabase.co/storage/v1/object/public/imlogo-audios/apps/Imlogo.exe'
 }
